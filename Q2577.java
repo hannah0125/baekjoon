1341b5fcf9ba84900ff3dee0bad6de6b1d17469b
hannah0125 <71058551+hannah0125@users.noreply.github.com>
2021-08-06 17:04:33 +0900

    각 자리 숫자 개수 세기

---
+++ Q2577.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Q2577 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        int A = scan.nextInt();
+        int B = scan.nextInt();
+        int C = scan.nextInt();
+
+        int num = A * B * C;
+
+        String NUM = Integer.toString(num);
+
+        for(int i = 0; i < 10; i++) {
+            int count = 0;
+            for(int j = 0; j < NUM.length(); j++) {
+                if((NUM.charAt(j) - '0') == i){
+                    count++;
+                }
+            }
+            System.out.println(count);
+        }
+
+    }
+}
